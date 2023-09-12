54a4f9519edc157016b0ac7907c4ca73b7f25f22
sensei07 <atomkiv17@gmail.com>
2023-09-12 22:12:11 +0300

    Webpack configuration

---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+import HTMLWebpackPlugin from 'html-webpack-plugin'
+import path from 'path'
+import webpack from 'webpack'
+
+const config: webpack.Configuration = {
+	mode: 'development',
+	entry: path.resolve(__dirname, 'src', 'index.ts'),
+	output: {
+		filename: '[name].[contenthash].js',
+		path: path.resolve(__dirname, 'build'),
+		clean: true,
+	},
+	plugins: [
+		new webpack.ProgressPlugin(),
+		new HTMLWebpackPlugin({
+			template: path.resolve(__dirname, 'public', 'index.html'),
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+}
+
+export default config
