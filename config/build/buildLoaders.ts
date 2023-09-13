530606dd5ce38355a5c281abfa36a82696a3d97c
sensei07 <atomkiv17@gmail.com>
2023-09-13 14:18:19 +0300

    React connection and css settings in webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,24 @@
 import webpack from 'webpack'
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+	const cssLoader = {
+		test: /\.s[ac]ss$/i,
+		use: [
+			// Creates `style` nodes from JS strings
+			'style-loader',
+			// Translates CSS into CommonJS
+			'css-loader',
+			// Compiles Sass to CSS
+			'sass-loader',
+		],
+	}
+
+	// if not using TS - install babel-loader
 	const typescriptLoader = {
 		test: /\.tsx?$/,
 		use: 'ts-loader',
 		exclude: /node_modules/,
 	}
 
-	return [typescriptLoader]
+	return [typescriptLoader, cssLoader]
 }
