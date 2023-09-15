fbf48155dc6368f8bd494e4eb97b2c9325f23af3
sensei07 <atomkiv17@gmail.com>
2023-09-15 19:31:55 +0300

    Babel. Extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -10,6 +10,26 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 		exclude: /node_modules/,
 	}
 
+	const babelLoader = {
+		test: /\.(?:js|jsx|tsx)$/,
+		exclude: /node_modules/,
+		use: {
+			loader: 'babel-loader',
+			options: {
+				presets: ['@babel/preset-env'],
+				plugins: [
+					[
+						'i18next-extract',
+						{
+							locales: ['en', 'uk'],
+							keyAsDefaultValue: true,
+						},
+					],
+				],
+			},
+		},
+	}
+
 	const cssLoader = {
 		test: /\.s[ac]ss$/i,
 		use: [
@@ -43,5 +63,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 		use: ['@svgr/webpack'],
 	}
 
-	return [typescriptLoader, cssLoader, fileLoader, svgLoader]
+	return [babelLoader, typescriptLoader, cssLoader, fileLoader, svgLoader]
 }
