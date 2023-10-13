ae4cefaf0d8d1c4a0a67cbc5d59075f7ce02256c
sensei07 <atomkiv17@gmail.com>
2023-10-13 12:32:41 +0300

    add redux-toolkit store, add first entity

---
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -0,0 +1,15 @@
+import { Provider } from 'react-redux';
+import { ReactNode } from 'react';
+import { createReduxStore } from 'app/providers/StoreProvider/config/config';
+import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+import { DeepPartial } from '@reduxjs/toolkit';
+
+interface StoreProviderProps {
+    children?: ReactNode;
+    initialState?: DeepPartial<StateSchema>
+}
+
+export const StoreProvider = ({ children, initialState }: StoreProviderProps) => {
+    const store = createReduxStore(initialState as StateSchema);
+    return <Provider store={store}>{children}</Provider>;
+};
