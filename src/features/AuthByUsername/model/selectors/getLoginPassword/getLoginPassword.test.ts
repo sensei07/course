b4dfaba44ef9c89200df39baed05bd99468c3419
sensei07 <atomkiv17@gmail.com>
2023-11-24 14:42:57 +0200

    add tests for login module

---
+++ src/features/AuthByUsername/model/selectors/getLoginPassword/getLoginPassword.test.ts
@@ -0,0 +1,16 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginPassword } from './getLoginPassword';
+
+describe('getLoginIsLoading.test', () => {
+    test('should return password', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: { password: 'password' },
+        };
+        expect(getLoginPassword(state as StateSchema)).toEqual('password');
+    });
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginPassword(state as StateSchema)).toEqual('');
+    });
+});
